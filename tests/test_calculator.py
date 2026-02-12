8d51318a93a08800019764a73bf5ea3f225129da
pizzadogsquared <elijahsprouse1806@gmail.com>
2026-02-11 21:56:28 -0700

    First commit

---
+++ tests/test_calculator.py
@@ -0,0 +1,15 @@
+import unittest
+from src.calculator import add, subtract, divide 
+class TestCalculator(unittest.TestCase):
+   def test_add(self):
+    self.assertEqual(add(2, 3), 5)
+    self.assertEqual(add(-1, 1), 0)
+    self.assertEqual(add(-1, -1), -2)
+def test_subtract(self):
+  pass
+  # COMPLETE HERE
+def test_divide(self):
+  pass
+  # COMPLETE HERE
+if __name__ == '__main__':
+   unittest.main()
